f2f3881ed9978034267462f2369268329e24e0d8
Robin Ochoa <robin8a@gmail.com>
2023-01-23 17:21:22 -0500

    Working on lambda

--- 00_DOCUMENTATION/001_ALGORITMS_AND_ML/kio-ucc-piscicultura-appetite-prediction-water-column.py
+++ 00_DOCUMENTATION/001_ALGORITMS_AND_ML/kio-ucc-piscicultura-appetite-prediction-water-column.py
@@ -21,7 +21,7 @@ def lambda_handler(event, context):
     print("Labels detected: " + str(detected_labels))
 
     count_detected_label_in_columns_json = count_fishes_in_column(detected_labels)
-    print("Count labels detected on column: " + str(count_detected_label_in_columns))
+    print("Count labels detected on column: " + str(count_detected_label_in_columns_json))
 
     
     
@@ -59,27 +59,27 @@ def count_fishes_in_column(detected_labels):
     count_fishes_middle = 0
     count_fishes_bottom = 0
 
-    for label in DATA_JSON_DETECT_LABELS:
+    for label in detected_labels:
         if (label['Name'] == 'Fish'):
-        count_fishes = len(label['Instances'])
-        print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
-        print ("Instances:")
-        for instance in label['Instances']:
-            print ("  Bounding box")
-            print ("    Top: " + str(instance['BoundingBox']['Top']))
-            if (float(instance['BoundingBox']['Top']) < 0.33):
-                count_fishes_top += 1
-            if (float(instance['BoundingBox']['Top']) > 0.33 and float(instance['BoundingBox']['Top']) < 0.66):
-                count_fishes_middle += 1
-            if (float(instance['BoundingBox']['Top']) > 0.66 and float(instance['BoundingBox']['Top']) < 1):
-                count_fishes_bottom += 1
-            print ("  Confidence: " + str(instance['Confidence']))
-            print()
+            count_fishes = len(label['Instances'])
+            print ("Label: " + label['Name'])
+            print ("Confidence: " + str(label['Confidence']))
+            print ("Instances:")
+            for instance in label['Instances']:
+                print ("  Bounding box")
+                print ("    Top: " + str(instance['BoundingBox']['Top']))
+                if (float(instance['BoundingBox']['Top']) < 0.33):
+                    count_fishes_top += 1
+                if (float(instance['BoundingBox']['Top']) > 0.33 and float(instance['BoundingBox']['Top']) < 0.66):
+                    count_fishes_middle += 1
+                if (float(instance['BoundingBox']['Top']) > 0.66 and float(instance['BoundingBox']['Top']) < 1):
+                    count_fishes_bottom += 1
+                print ("  Confidence: " + str(instance['Confidence']))
+                print()
 
-        print ('count_fishes: ' + str(count_fishes))
-        print ('count_fishes_top: ' + str(count_fishes_top))
-        print ('count_fishes_middle: ' + str(count_fishes_middle))
-        print ('count_fishes_bottom: ' + str(count_fishes_bottom))
+    print ('count_fishes: ' + str(count_fishes))
+    print ('count_fishes_top: ' + str(count_fishes_top))
+    print ('count_fishes_middle: ' + str(count_fishes_middle))
+    print ('count_fishes_bottom: ' + str(count_fishes_bottom))
 
-    return { count_fishes:  str(count_fishes), count_fishes_top: str(count_fishes_top)}
+    return { "count_fishes":  str(count_fishes), "count_fishes_top": str(count_fishes_top), "count_fishes_middle": str(count_fishes_middle), "count_fishes_bottom": str(count_fishes_bottom)}
